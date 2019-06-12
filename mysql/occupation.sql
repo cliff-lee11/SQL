d55c56e74bbb118ae38e88c8c8b372f4df6c2dc1
cliff-lee11 <40394653+cliff-lee11@users.noreply.github.com>
2019-06-12 17:35:27 +0900

    occupation 스키마 & 예제 데이터 dump file (Mysql)

---
+++ mysql/occupation.sql
@@ -0,0 +1,29 @@
+/*
+SQLyog Ultimate v9.51 
+MySQL - 5.6.36 : Database - rcv
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `occupations` */
+
+DROP TABLE IF EXISTS `occupations`;
+
+CREATE TABLE `occupations` (
+  `name` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `occupation` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `occupations` */
+
+insert  into `occupations`(`name`,`occupation`) values ('Samantha','Doctor'),('Julia','Actor'),('Maria','Actor'),('Meera','Singer'),('Ashely','Professor'),('Ketty','Professor'),('Christeen','Professor'),('Jane','Actor'),('Jenny','Doctor'),('Priya','Singer');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
